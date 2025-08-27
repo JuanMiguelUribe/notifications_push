8a1bff86ea5c3dc00ffc58988c01f6c818ff0e28
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-27 18:14:45 -0500

    se lee el bloc para saber el estado de permisos de la aplicacion

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notifications_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,9 +9,17 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Permisos"),
+        title: context.select(
+          (NotificationsBloc bloc) =>
+              Text("${bloc.state.status} Notifications"),
+        ),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+          IconButton(
+            onPressed: () {
+              context.read<NotificationsBloc>().requestPermission();
+            },
+            icon: const Icon(Icons.settings),
+          ),
         ],
       ),
       body: HomeView(),
