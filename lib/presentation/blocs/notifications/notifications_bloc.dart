e67c027c8a5480036e636869cbba918f3bf1754f
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-28 15:48:02 -0500

    se creo la funcion de notificationsstatuschanged

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:notifications_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -9,9 +11,26 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(NotificationsState()) {
-    on<NotificationsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<NotificationStatusChanged>(_notificationsStatusChanged);
+    _initialStatusCheck();
+  }
+  // initialize firebase cloud messaging
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _notificationsStatusChanged(
+    NotificationStatusChanged event,
+    Emitter<NotificationsState> emit,
+  ) {
+    emit(state.copyWidth(status: event.status));
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
   }
 
   void requestPermission() async {
@@ -24,6 +43,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
-    settings.authorizationStatus;
+
+    add(NotificationStatusChanged(settings.authorizationStatus));
   }
 }
